af061b72f0e9d6020e818154ed44237f4ad66899
deepak.deopura@gmail.com <Ohio*1234>
2022-03-15 12:11:27 -0700

    First commit

---
+++ dd_server.py
@@ -0,0 +1,32 @@
+import grpc
+from concurrent import futures
+import time
+import dd_grps_sample_pb2_grpc as pb2_grpc
+import dd_grps_sample_pb2 as pb2
+
+
+class ddgreetingService(pb2_grpc.ddgreeting):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def GetServerResponse(self, request, context):
+
+        # get the string from the incoming request
+        message = request.message
+        result = f'Hello Deepak Deopura,I am up and running received "{message}" message from you'
+        result = {'message': result, 'received': True}
+
+        return pb2.MessageResponse(**result)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pb2_grpc.add_ddgreetingServicer_to_server(ddgreetingService(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
