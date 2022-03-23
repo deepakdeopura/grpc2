aeaa262669283da2d36d3fd1ce3b35d19529b73d
deepak.deopura@gmail.com <Ohio*1234>
2022-03-22 23:36:04 -0700

    commit 1

--- dd_client.py
+++ dd_client.py
@@ -1,10 +1,10 @@
 import grpc
 import dd_grps_sample_pb2_grpc as pb2_grpc
 import dd_grps_sample_pb2 as pb2
-#import unary_pb2 as pb2
 
 
-class UnaryClient(object):
+
+class ddClient(object):
     """
     Client for gRPC functionality
     """
@@ -13,7 +13,7 @@ class UnaryClient(object):
         self.host = 'localhost'
         self.server_port = 50051
 
-        # instantiate a channel
+       
         self.channel = grpc.insecure_channel(
             '{}:{}'.format(self.host, self.server_port))
 
@@ -21,15 +21,13 @@ class UnaryClient(object):
         self.stub = pb2_grpc.ddgreetingStub(self.channel)
 
     def get_url(self, message):
-        """
-        Client function to call the rpc for GetServerResponse
-        """
+
         message = pb2.Message(message=message)
         print(f'{message}')
         return self.stub.GetServerResponse(message)
 
 
 if __name__ == '__main__':
-    client = UnaryClient()
-    result = client.get_url(message="Hello Deepak Deopura, you there on server?")
+    client = ddClient()
+    result = client.get_url(message="Hello GRPC server")
     print(f'{result}')
--- dd_server.py
+++ dd_server.py
@@ -14,7 +14,7 @@ class ddgreetingService(pb2_grpc.ddgreeting):
 
         # get the string from the incoming request
         message = request.message
-        result = f'Hello Deepak Deopura,I am up and running received "{message}" message from you'
+        result = f'Hello  GRPC Client ,I am GRPC server up and running received "{message}" message from you'
         result = {'message': result, 'received': True}
 
         return pb2.MessageResponse(**result)
